1976d8a634c88a1f5c8e1c707e64d84ed47e48f7
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-01-30 15:04:14 +0000

    EODHP-10 - remove docker deployment file

--- docker_deploy.sh
+++
@@ -1,5 +0,0 @@
-docker build -t eodhp_website .
-docker tag eodhp_website hcollingwoodtpz/eodhp_website:0.1.4
-docker push hcollingwoodtpz/eodhp_website:0.1.4
-
-#docker run --rm -p 8000:8000 eodhp_web_presence
