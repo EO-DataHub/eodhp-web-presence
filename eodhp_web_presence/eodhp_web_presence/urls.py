c6f0cf3221fd870eeaa9b071e2f5dc9b15f99dea
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-02-06 15:25:35 +0000

    EODHP-11 - update isort

--- eodhp_web_presence/eodhp_web_presence/urls.py
+++ eodhp_web_presence/eodhp_web_presence/urls.py
@@ -1,11 +1,10 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
+from search import views as search_views
 from wagtail import urls as wagtail_urls
 from wagtail.admin import urls as wagtailadmin_urls
 from wagtail.documents import urls as wagtaildocs_urls
-
-from search import views as search_views
 
 urlpatterns = [
     path("django-admin/", admin.site.urls),
