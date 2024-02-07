459ea7151f4473aa03d1905411bc16d7a4d12ad0
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-02-07 16:04:53 +0000

    EODHP-10 - test formatting

--- eodhp_web_presence/home/tests/test_home.py
+++ eodhp_web_presence/home/tests/test_home.py
@@ -3,5 +3,5 @@ from django.test import TestCase
 
 class TestHome(TestCase):
     def test_status_code__success(self):
-        response = self.client.get('/')
+        response = self.client.get("/")
         self.assertEqual(response.status_code, 200)
