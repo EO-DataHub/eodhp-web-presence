5128847178195d67ccbbcf13cb65a2fdf7bc4ff3
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-04-02 09:19:16 +0100

    EODHP-256 - update settings

--- eodhp_web_presence/eodhp_web_presence/settings.py
+++ eodhp_web_presence/eodhp_web_presence/settings.py
@@ -56,13 +56,15 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    # WhiteNoise Middleware above all but below Security
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    "django.middleware.security.SecurityMiddleware",
     "wagtail.contrib.redirects.middleware.RedirectMiddleware",
 ]
 
@@ -159,10 +161,10 @@ STATICFILES_DIRS = [
     os.path.join(PROJECT_DIR, "eodhp_web_presence", "static"),
 ]
 
-# ManifestStaticFilesStorage is recommended in production, to prevent outdated
-# JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
+# django.contrib.staticfiles.storage.ManifestStaticFilesStorage is recommended in production, to
+# prevent outdated JavaScript / CSS assets being served from cache (e.g. after a Wagtail upgrade).
 # See https://docs.djangoproject.com/en/5.0/ref/contrib/staticfiles/#manifeststaticfilesstorage
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = "/static/"
