761e7853b5513380424cf237ec272c7ed7f76271
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-04-02 14:35:46 +0100

    EODHP-256 - update tests

--- eodhp_web_presence/home/tests/test_home.py
+++ eodhp_web_presence/home/tests/test_home.py
@@ -1,12 +1,4 @@
-import os
-
 from django.test import TestCase
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eodhp_web_presence.settings")
-
-import django  # noqa:E402
-
-django.setup()
 
 
 class TestHome(TestCase):
