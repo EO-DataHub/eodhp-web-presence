a5a913ef947861a34536694f545f3b6ab0e07812
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-04-03 15:59:47 +0100

    EODHP-256 - add line break

--- eodhp_web_presence/eodhp_web_presence/settings.py
+++ eodhp_web_presence/eodhp_web_presence/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+
 import logging
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
