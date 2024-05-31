096b20c83f023673f659bb44441843b7246ccce4
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-05-31 14:27:04 +0100

    EODHP-320 - linting

--- eodhp_web_presence/eodhp_web_presence/middleware/middleware.py
+++ eodhp_web_presence/eodhp_web_presence/middleware/middleware.py
@@ -1,9 +1,8 @@
 class CacheHeaderMiddleware:
-
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
         response = self.get_response(request)
-        response['Cache-Control'] = f"max-age=300"
+        response["Cache-Control"] = "max-age=300"
         return response
