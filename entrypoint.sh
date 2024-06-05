8a119956c3fc68726e200c2c725cd35fe4b5a119
james-hinton <james.hinton@live.co.uk>
2024-06-05 12:22:27 +0100

    Added wait_for_it script for webpack stats

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
+check_webpack_ready() {
+    echo "Checking for webpack-stats.json..."
+    for i in {1..10}; do
+        if [ -f "/app/webpack-stats.json" ]; then
+            echo "webpack-stats.json found. Proceeding..."
+            return 0
+        else
+            echo "Waiting for webpack-stats.json to become available..."
+            sleep 2
+        fi
+    done
+    echo "Failed to find webpack-stats.json. Exiting..."
+    exit 1
+}
+
 if [ "$DJANGO_ENV" == "development" ]; then
+    # Wait for webpack to be ready
+    check_webpack_ready
     python manage.py runserver 0.0.0.0:8000
 else
     python manage.py migrate
