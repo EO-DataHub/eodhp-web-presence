a0c365bcd84b52c64e744df85ede133e10bc59af
james-hinton <james.hinton@live.co.uk>
2024-06-05 13:07:16 +0100

    Refactored entrypoint.sh script to include a new function run_django_prereqs() for running Django prerequisites before starting the server

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,6 +5,7 @@ check_webpack_ready() {
     for i in {1..10}; do
         if [ -f "/app/webpack-stats.json" ]; then
             echo "webpack-stats.json found. Proceeding..."
+            sleep 8
             return 0
         else
             echo "Waiting for webpack-stats.json to become available..."
@@ -15,12 +16,19 @@ check_webpack_ready() {
     exit 1
 }
 
+run_django_prereqs() {
+    echo "Running Django prerequisites..."
+    python manage.py migrate
+    python manage.py collectstatic --no-input
+}
+
 if [ "$DJANGO_ENV" == "development" ]; then
     # Wait for webpack to be ready
     check_webpack_ready
+    # Run Django prerequisites
+    run_django_prereqs
     python manage.py runserver 0.0.0.0:8000
 else
-    python manage.py migrate
-    python manage.py collectstatic --no-input
+    run_django_prereqs
     exec gunicorn eodhp_web_presence.wsgi:application --bind 0.0.0.0:8000 --timeout=30 --worker-class=gevent --workers=4
 fi
