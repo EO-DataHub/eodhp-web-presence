c41bd824cb5ca6bf244828525da11b972db78457
james-hinton <james.hinton@live.co.uk>
2024-06-14 14:31:49 +0100

    Fixed formatting issues

--- eodhp_web_presence/home/views.py
+++ eodhp_web_presence/home/views.py
@@ -1,15 +1,15 @@
 """
-Still a proof of concept. 
+Proof of concept.
 
-This view is intended to be used to generate temporary credentials for a user to access their associated S3 access point.
+This view is intended to be used to generate temporary credentials for a user
+to access their associated S3 access point.
 
-To complete this we need to ensure that we can retrieve the following from the users session:
+To complete this we need to ensure that we can
+retrieve the following from the users session:
 - username
-- access_point_arn 
-- workspace_access_role_arn 
-
+- access_point_arn
+- workspace_access_role_arn
 """
-
 import logging
 
 import boto3
@@ -19,7 +19,9 @@ logger = logging.getLogger(__name__)
 
 
 def check_user_has_access_point_access(username):
-    access_point_arn = f"arn:aws:s3:eu-west-2:#:accesspoint/eodhp-dev-y4jfxod4-{username}-s3"  # TODO: Hardcoded for now, in future this will come from the users session.
+    
+    # TODO: Hardcoded for now, in future this will come from the users session.
+    access_point_arn = f"arn:aws:s3:eu-west-2:#:accesspoint/eodhp-dev-y4jfxod4-{username}-s3"
     s3_client = boto3.client(
         "s3",
     )
@@ -32,16 +34,20 @@ def get_temp_credentials(request):
 
     if not username:
         return JsonResponse({"error": "The username parameter is required."}, status=400)
+    
+    # TODO: Hardcoded for now, in future this will come from the users session.
+    workspace_access_role_arn = "arn:aws:iam::#:role/jh-s3-workspace-access"
 
-    workspace_access_role_arn = "arn:aws:iam::#:role/#"  # TODO: Hardcoded for now, in future this will come from the users session.
-    session_name = f"WorkspaceSession-{username}"  # Attaching the username to the session name to make it auditable
+    # Attaching the username to the session name to make it auditable
+    session_name = f"WorkspaceSession-{username}"
 
     try:
         sts_client = boto3.client("sts")
         assumed_role = sts_client.assume_role(
             RoleArn=workspace_access_role_arn,
             RoleSessionName=session_name,
-            DurationSeconds=3600,  # TODO: We need to allow role permissions to extend longer than 3600
+            # TODO: We need to allow role permissions to extend longer than 3600
+            DurationSeconds=3600,
         )
         credentials = assumed_role["Credentials"]
 
