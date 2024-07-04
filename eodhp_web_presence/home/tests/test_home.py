4a4cfad5cdbd53f8ecb8f134c279c487d3f58c96
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-07-04 12:28:15 +0100

    EODHP-368 - update dependencies

--- eodhp_web_presence/home/tests/test_home.py
+++ eodhp_web_presence/home/tests/test_home.py
@@ -5,3 +5,4 @@ class TestHome(TestCase):
     def test_status_code__success(self):
         home_page_get = self.client.get("/")
         assert home_page_get.status_code == 200
+        assert b"Home" in home_page_get.content
