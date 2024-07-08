2b6a5f7aca74377aa137e53aac09d2fc87946056
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-07-08 17:30:16 +0100

    EODHP-209 - fix URLs

--- eodhp_web_presence/home/urls.py
+++ eodhp_web_presence/home/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from .views import catalogue_page_view, search_topics, search_topics_blank
 
 urlpatterns = [
-    path("", catalogue_page_view, name="home/catalogue_page"),
     path("search_items", search_topics, name="search_topics"),
     path("search_items_blank", search_topics_blank, name="search_topics_blank"),
 ]
