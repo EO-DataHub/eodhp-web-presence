3cfd25fe83bdc8f5de3ec1f81ef494cd49bd1387
Hannah Collingwood <hannah.collingwood@telespazio.com>
2024-07-09 15:40:20 +0100

    EODHP-209 - refactor JS

--- eodhp_web_presence/eodhp_web_presence/js-src/js/support.js
+++ eodhp_web_presence/eodhp_web_presence/js-src/js/support.js
@@ -1,24 +1,6 @@
 import $ from 'jquery'
 
-const filterItemsIndex = async() => {
-    const input = document.getElementById('searchBoxIndex').value;
-    if (input === '') {
-        document.getElementById('supportTopicSearchResults').innerHTML = '';
-    }
-    else {
-        const request = new XMLHttpRequest();
-        await request.open('GET', '/support/search_items?query=' + encodeURIComponent(input), true);
-        request.onreadystatechange = function() {
-            if (request.readyState == 4 && request.status == 200) {
-                document.getElementById('supportTopicSearchResults').innerHTML = request.responseText;
-            }
-        }
-        await request.send()
-    }
-}
-
-const filterItemsArea = async() => {
-    const input = document.getElementById('searchBoxArea').value;
+const runSearch = async(input) => {
     const request = new XMLHttpRequest();
     await request.open('GET', '/support/search_items?query=' + encodeURIComponent(input), true);
     request.onreadystatechange = function() {
@@ -27,6 +9,21 @@ const filterItemsArea = async() => {
         }
     }
     await request.send()
+}
+
+function filterItemsIndex() {
+    const input = document.getElementById('searchBoxIndex').value;
+    if (input === '') {
+        document.getElementById('supportTopicSearchResults').innerHTML = '';
+    }
+    else {
+        runSearch(input);
+    }
+}
+
+function filterItemsArea() {
+    const input = document.getElementById('searchBoxArea').value;
+    runSearch(input);
 }
 
 $(() => {
