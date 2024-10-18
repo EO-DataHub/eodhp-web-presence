807a8e7ea22b5976fb04c7e369ac6af0159b929e
Mark Small <mark.small@telespazio.com>
2024-10-18 11:00:38 +0100

    fix(assets): Prevent importing styles multiple times
    Looking at the `<head />` section, I noticed the styles were being
    imported multiple times, which while it didn't break anything, made
    it harder to turn styles on/off when testing something in the browser.
    It seems to be caused by the `entrypoint.js` script importing multiple
    SCSS files, so I wrapped them all in a single file and import only
    that one and it appears to have fixed the issue.
    
    IssueID EODHP-596

--- assets/entrypoint.js
+++ assets/entrypoint.js
@@ -1,9 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
-import './scss/custom.scss';
-import './scss/fira.scss';
-import './scss/menu.scss';
-import './scss/footer.scss';
-import './scss/home.scss';
+import './scss/main.scss';
+
 import 'bootstrap';
 import './js/support.js';
 import './js/version.js';
