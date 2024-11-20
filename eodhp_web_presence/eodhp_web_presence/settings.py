dbea201e26ae1a0ebc3358640164111f0e977ff0
James Hinton <63542818+james-hinton@users.noreply.github.com>
2024-11-20 10:19:09 +0000

    Update default bucket name in settings

--- eodhp_web_presence/eodhp_web_presence/settings.py
+++ eodhp_web_presence/eodhp_web_presence/settings.py
@@ -249,7 +249,7 @@ STORAGES = {
 
 if env("USE_S3", default=False, cast=bool):
     # Set the required AWS credentials
-    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default="eodhp-static-web-artefacts")
+    AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME", default="static-web-artefacts-eodhp")
     AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", default="eu-west-2")
 
     # Set the media files locations relative to the S3 bucket
