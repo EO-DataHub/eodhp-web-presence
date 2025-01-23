4d675ed9aec2def42567acc133f8a2f34d534080
Steven Gillies <steven.gillies@telespazio.com>
2025-01-23 09:44:28 +0000

    fixed middleware unit tests and removed ones that rely on OPA middleware

--- eodhp_web_presence/accounts/tests/test_middleware.py
+++ eodhp_web_presence/accounts/tests/test_middleware.py
@@ -7,27 +7,25 @@ from django.contrib.auth.models import AnonymousUser
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.http import HttpResponse
 from django.test import RequestFactory, TestCase
+from django.test.utils import override_settings
 
-from ..middleware import ClaimsMiddleware, OPAAuthorizationMiddleware
+from ..middleware import ClaimsMiddleware
 from ..models import User
 from ..tokens import UserClaims
 
 factory = RequestFactory()
 
 
-def create_bearer_token():
-    return "Bearer " + jwt.encode(
-        {
-            "preferred_username": "test-user",
-            "realm_access": {
-                "roles": ["hub_user"],
-            },
-        },
-        "secret",
-        algorithm="HS256",
-    )
-
-
+@override_settings(
+    OIDC_CLAIMS={
+        "ENABLED": True,
+        "USERNAME_PATH": "username",
+        "ROLES_PATH": "roles",
+        "SUPERUSER_ROLE": "admin",
+        "MODERATOR_ROLE": "moderator",
+        "EDITOR_ROLE": "editor",
+    }
+)
 class ClaimsMiddlewareTestCase(TestCase):
     def setUp(self):
         self.view = mock.MagicMock(return_value=HttpResponse("OK", status=200))
@@ -40,8 +38,14 @@ class ClaimsMiddlewareTestCase(TestCase):
         )
 
     def test_call__valid_token__auth(self):
-        bearer_token = create_bearer_token()
-        request = factory.get("/", headers={"Authorization": bearer_token})
+        auth_header = "Bearer " + jwt.encode(
+            {
+                "username": "test-user",
+            },
+            "secret",
+            algorithm="HS256",
+        )
+        request = factory.get("/", headers={"Authorization": auth_header})
 
         response = self.middleware(request)
 
@@ -50,7 +54,6 @@ class ClaimsMiddlewareTestCase(TestCase):
             request.claims,
             UserClaims(
                 username="test-user",
-                roles=["hub_user"],
             ),
         )
         self.assertIsInstance(request.user, User)
@@ -86,60 +89,17 @@ class ClaimsMiddlewareTestCase(TestCase):
 
     def test_call__auth_user_doesnt_match_claims__log_in_claims_user(self):
         auth_user = User.objects.create_user(username="user-1")
-        bearer_token = create_bearer_token()
-        request = factory.get("/", user=auth_user, headers={"Authorization": bearer_token})
+        auth_header = "Bearer " + jwt.encode(
+            {
+                "username": "test-user",
+            },
+            "secret",
+            algorithm="HS256",
+        )
+        request = factory.get("/", user=auth_user, headers={"Authorization": auth_header})
 
         response = self.middleware(request)
 
         self.assertEqual(response.status_code, HTTPStatus.OK)
         self.assertEqual(request.claims.username, "test-user")
         self.assertEqual(request.user.username, "test-user")
-
-
-@mock.patch.object(OPAAuthorizationMiddleware, "is_allowed")
-class OPAAuthorizationMiddlewareTestCase(TestCase):
-    def setUp(self):
-        self.view = mock.MagicMock(return_value=HttpResponse("OK", status=200))
-        self.middleware = SessionMiddleware(
-            get_response=AuthenticationMiddleware(
-                get_response=ClaimsMiddleware(
-                    get_response=OPAAuthorizationMiddleware(
-                        self.view, opa_client_url="http://localhost:8181"
-                    ),
-                )
-            )
-        )
-
-    def test_authorized_path__unauthenticated__return_200(self, is_allowed: mock.MagicMock):
-        request = factory.get("/")
-        is_allowed.return_value = True
-
-        response = self.middleware(request)
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_authorized_path__authenticated__return_200(self, is_allowed: mock.MagicMock):
-        bearer_token = create_bearer_token()
-        request = factory.get("/", headers={"Authorization": bearer_token})
-        is_allowed.return_value = True
-
-        response = self.middleware(request)
-
-        self.assertEqual(response.status_code, HTTPStatus.OK)
-
-    def test_unauthorized_path__unauthenticated__return_401(self, is_allowed: mock.MagicMock):
-        request = factory.get("/")
-        is_allowed.return_value = False
-
-        response = self.middleware(request)
-
-        self.assertEqual(response.status_code, HTTPStatus.UNAUTHORIZED)
-
-    def test_unauthorized_path__authenticated__return_403(self, is_allowed: mock.MagicMock):
-        bearer_token = create_bearer_token()
-        request = factory.get("/", headers={"Authorization": bearer_token})
-        is_allowed.return_value = False
-
-        response = self.middleware(request)
-
-        self.assertEqual(response.status_code, HTTPStatus.FORBIDDEN)
