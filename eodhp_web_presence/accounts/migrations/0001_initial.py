cd4ffebc1008c9835a67f4f15f01e1e0640324e7
james-hinton <james.hinton@live.co.uk>
2025-01-31 15:48:14 +0000

    lint: fix further issues with migration formatting

--- eodhp_web_presence/accounts/migrations/0001_initial.py
+++ eodhp_web_presence/accounts/migrations/0001_initial.py
@@ -32,7 +32,10 @@ class Migration(migrations.Migration):
                     "is_superuser",
                     models.BooleanField(
                         default=False,
-                        help_text="Designates that this user has all permissions without explicitly assigning them.",
+                        help_text=(
+                            "Designates that this user has all permissions without "
+                            "explicitly assigning them."
+                        ),
                         verbose_name="superuser status",
                     ),
                 ),
@@ -71,19 +74,27 @@ class Migration(migrations.Migration):
                     "is_active",
                     models.BooleanField(
                         default=True,
-                        help_text="Designates whether this user should be treated as active. Unselect this instead of deleting accounts.",
+                        help_text=(
+                            "Designates whether this user should be treated as active. "
+                            "Unselect this instead of deleting accounts."
+                        ),
                         verbose_name="active",
                     ),
                 ),
                 (
                     "date_joined",
-                    models.DateTimeField(default=django.utils.timezone.now, verbose_name="date joined"),
+                    models.DateTimeField(
+                        default=django.utils.timezone.now, verbose_name="date joined"
+                    ),
                 ),
                 (
                     "groups",
                     models.ManyToManyField(
                         blank=True,
-                        help_text="The groups this user belongs to. A user will get all permissions granted to each of their groups.",
+                        help_text=(
+                            "The groups this user belongs to. "
+                            "A user will get all permissions granted to each of their groups."
+                        ),
                         related_name="user_set",
                         related_query_name="user",
                         to="auth.group",
