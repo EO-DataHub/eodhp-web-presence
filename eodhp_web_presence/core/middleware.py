59140118fdc846bd084fa985156b6a0fa8a1e67a
hcollingwood <hannah.collingwood@telespazio.com>
2025-02-25 16:31:34 +0000

    EODHP-1180 - add header

--- eodhp_web_presence/core/middleware.py
+++ eodhp_web_presence/core/middleware.py
@@ -10,6 +10,7 @@ class HeaderMiddleware:
 
     def __call__(self, request):
         response = self.get_response(request)
+        response["Very"] = "Cookie"
         if not IS_PROD:
             response["X-Robots-Tag"] = "noindex"
         return response
