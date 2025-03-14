2be9de2fc9ce5d1602fe8734d31f1eb443077034
james-hinton <james.hinton@live.co.uk>
2025-03-14 12:16:18 +0000

    Update notebooks URL to use settings configuration in context_processors.py

--- eodhp_web_presence/core/context_processors.py
+++ eodhp_web_presence/core/context_processors.py
@@ -3,7 +3,7 @@ from eodhp_web_presence import settings
 
 def menu_links(request):
     return {
-        "notebooks_url": "https://{workspace}.dev.eodatahub-workspaces.org.uk/notebooks",
+        "notebooks_url": settings.NOTEBOOKS["url"],
         "resource_catalogue_url": "/catalogue",
         "workspaces_ui_url": "/workspaces",
     }
