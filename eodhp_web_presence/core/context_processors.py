5192a79bcf77679bcc51fcdbcb75ce942e17a2a1
David Tamayo <dtamayo@sparkgeo.com>
2026-01-26 08:24:51 -0500

    RC-122 add workspace as query param to integrate RC to main ui

--- eodhp_web_presence/core/context_processors.py
+++ eodhp_web_presence/core/context_processors.py
@@ -1,9 +1,16 @@
 from eodhp_web_presence import settings
 
 
+def get_workspaces_ui_url(request):
+    workspace = request.GET.get("workspace")
+    if workspace:
+        return f"/workspaces?workspace={workspace}"
+    return "/workspaces"
+
+
 def menu_links(request):
     return {
         "notebooks_url": settings.NOTEBOOKS["url"],
         "resource_catalogue_url": "/catalogue",
-        "workspaces_ui_url": "/workspaces",
+        "workspaces_ui_url": get_workspaces_ui_url(request),
     }
