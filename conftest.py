4038fa0f86455ebbcdc2ffcd5c93b2773a082f08
Jakub Recman <jakub@oxidian.com>
2026-03-23 19:07:03 +0100

    Fix staticfiles override to use pytest-django settings fixture

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
-import django.conf
+import pytest
 
 
-def pytest_configure() -> None:
-    django.conf.settings.STORAGES = {
-        **django.conf.settings.STORAGES,
+@pytest.fixture(autouse=True)
+def _use_simple_staticfiles(settings: object) -> None:
+    """Use plain StaticFilesStorage in tests so collectstatic is not required."""
+    settings.STORAGES = {
+        **settings.STORAGES,
         "staticfiles": {
             "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
         },
