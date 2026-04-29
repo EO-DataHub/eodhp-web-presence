18622712fa5ebdda562d938d91a945b6f46d9029
Jakub Recman <jakub@oxidian.com>
2026-04-29 17:22:34 +0200

    Fix inclusion tag path

--- eodhp_web_presence/home/templatetags/picture_tags.py
+++ eodhp_web_presence/home/templatetags/picture_tags.py
@@ -6,21 +6,21 @@ register = template.Library()
 ImageModel = get_image_model()
 
 
-@register.inclusion_tag("includes/pictures/content_image.html")
+@register.inclusion_tag("home/includes/pictures/content_image.html")
 def content_picture(image: ImageModel) -> dict[str, ImageModel]:
     return {"image": image}
 
 
-@register.inclusion_tag("includes/pictures/hero_image.html")
+@register.inclusion_tag("home/includes/pictures/hero_image.html")
 def hero_picture(image: ImageModel) -> dict[str, ImageModel]:
     return {"image": image}
 
 
-@register.inclusion_tag("includes/pictures/featured_card_image.html")
+@register.inclusion_tag("home/includes/pictures/featured_card_image.html")
 def featured_card_picture(image: ImageModel) -> dict[str, ImageModel]:
     return {"image": image}
 
 
-@register.inclusion_tag("includes/pictures/thumb_image.html")
+@register.inclusion_tag("home/includes/pictures/thumb_image.html")
 def thumb_picture(image: ImageModel) -> dict[str, ImageModel]:
     return {"image": image}
